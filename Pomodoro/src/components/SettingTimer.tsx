cf93125ca5ace56bad201d88f6f62499c9c4345a
MarvelousChester <karandeepsandhu2789@gmail.com>
2024-01-05 14:37:28 -0500

    Add decrementing timer button

--- Pomodoro/src/components/SettingTimer.tsx
+++ Pomodoro/src/components/SettingTimer.tsx
@@ -1,19 +1,27 @@
+import { useState } from "react";
 interface Props {
   minuteInit: number;
   secondInit: number;
 }
 
 const SettingTimer = ({ minuteInit, secondInit }: Props) => {
-  let minute = minuteInit;
-  let second = secondInit;
+  const [minute, setMinute] = useState(minuteInit);
+  const [second, setSecond] = useState(secondInit);
 
+  const handleDecrementClick = () => {
+    if (minute === 0) {
+      setMinute(60);
+    } else {
+      setMinute(minute - 1);
+    }
+  };
   return (
     <div>
       <div contentEditable="true">
         {minute}:{second}
       </div>
       <button>↑</button>
-      <button>↓</button>
+      <button onClick={handleDecrementClick}>↓</button>
     </div>
   );
 };
